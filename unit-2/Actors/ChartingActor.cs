53975c6720223e31bc62270524f94c628961fb67
phhlho <phillip.heyrman@gmail.com>
2015-11-18 22:34:04 -0600

    Unit 2.2 Complete - changed ChartingActor to a Receive type, adding 'Add Fake Series' behavior.

--- unit-2/Actors/ChartingActor.cs
+++ unit-2/Actors/ChartingActor.cs
@@ -6,7 +6,7 @@ using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : UntypedActor
+    public class ChartingActor : ReceiveActor
     {
         #region Messages
 
@@ -20,6 +20,19 @@ namespace ChartApp.Actors
             public Dictionary<string, Series> InitialSeries { get; private set; }
         }
 
+        /// <summary>
+        /// Add a new <see cref="Series"/> to the chart
+        /// </summary>
+        public class AddSeries
+        {
+            public AddSeries(Series series)
+            {
+                Series = series;
+            }
+
+            public Series Series { get; private set; }
+        }
+
         #endregion
 
         private readonly Chart _chart;
@@ -28,24 +41,25 @@ namespace ChartApp.Actors
         public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>())
         {
         }
-
         public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
-        }
 
-        protected override void OnReceive(object message)
-        {
-            if (message is InitializeChart)
-            {
-                var ic = message as InitializeChart;
-                HandleInitialize(ic);
-            }
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
         }
 
         #region Individual Message Type Handlers
 
+        private void HandleAddSeries(AddSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
+            {
+                _seriesIndex.Add(series.Series.Name, series.Series);
+                _chart.Series.Add(series.Series);
+            }
+        }
         private void HandleInitialize(InitializeChart ic)
         {
             if (ic.InitialSeries != null)
