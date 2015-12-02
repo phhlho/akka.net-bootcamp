d7d9b07280bdfd12660509b55ae5ed8ee4a250aa
Phillip Heyrman <phhlho@gmail.com>
2015-12-01 22:19:44 -0600

    Bulk adding files from previous machine - completed everything up to 2.3.

---
+++ unit-2/Actors/PerformanceCounterActor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    /// <summary>
+    /// Actor responsible for monitoring a specific <see cref="PerformanceCounter"/>
+    /// </summary>
+    public class PerformanceCounterActor : ReceiveActor
+    {
+        private readonly string _seriesName;
+        private readonly Func<PerformanceCounter> _performanceCounterGenerator;
+        private PerformanceCounter _counter;
+
+        private readonly HashSet<IActorRef> _subscriptions;
+        private readonly ICancelable _cancelPublishing;
+
+        public PerformanceCounterActor(string seriesName, Func<PerformanceCounter> performanceCounterGenerator)
+        {
+            _seriesName = seriesName;
+            _performanceCounterGenerator = performanceCounterGenerator;
+            _subscriptions = new HashSet<IActorRef>();
+            _cancelPublishing = new Cancelable(Context.System.Scheduler);
+
+            Receive<GatherMetrics>(m =>
+            {
+                //publish latest counter value to all subscribers
+                var metric = new Metric(_seriesName, _counter.NextValue());
+                foreach (var sub in _subscriptions)
+                    sub.Tell(metric);
+            });
+            Receive<SubscribeCounter>(sc =>
+            {
+                // add a subscription for this counter
+                // (it's parent's job to filter by counter types)
+                _subscriptions.Add(sc.Subscriber);
+            });
+            Receive<UnsubscribeCounter>(uc =>
+            {
+                // remove a subscription from this counter
+                _subscriptions.Remove(uc.Subscriber);
+            });
+        }
+
+        #region Actor lifecycle methods
+
+        protected override void PreStart()
+        {
+            //create a new instance of the performance counter
+            _counter = _performanceCounterGenerator();
+            Context.System.Scheduler.ScheduleTellRepeatedly(TimeSpan.FromMilliseconds(250), TimeSpan.FromMilliseconds(250), Self,
+                new GatherMetrics(), Self, _cancelPublishing);
+        }
+
+        protected override void PostStop()
+        {
+            try
+            {
+                //terminate the scheduled task
+                _cancelPublishing.Cancel(false);
+                _counter.Dispose();
+            }
+            catch
+            {
+                //don't care about additional "ObjectDisposed" exceptions
+            }
+            finally
+            {
+                base.PostStop();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ unit-2/Main.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ChartApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnCPU = new System.Windows.Forms.Button();
+            this.btnMemory = new System.Windows.Forms.Button();
+            this.btnDisk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sysChart
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.sysChart.ChartAreas.Add(chartArea4);
+            this.sysChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Name = "Legend1";
+            this.sysChart.Legends.Add(legend4);
+            this.sysChart.Location = new System.Drawing.Point(0, 0);
+            this.sysChart.Name = "sysChart";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.sysChart.Series.Add(series4);
+            this.sysChart.Size = new System.Drawing.Size(684, 446);
+            this.sysChart.TabIndex = 0;
+            this.sysChart.Text = "Add Series";
+            // 
+            // btnCPU
+            // 
+            this.btnCPU.Location = new System.Drawing.Point(567, 291);
+            this.btnCPU.Name = "btnCPU";
+            this.btnCPU.Size = new System.Drawing.Size(105, 33);
+            this.btnCPU.TabIndex = 1;
+            this.btnCPU.Text = "CPU (On)";
+            this.btnCPU.UseVisualStyleBackColor = true;
+            this.btnCPU.Click += new System.EventHandler(this.btnCPU_Click);
+            // 
+            // btnMemory
+            // 
+            this.btnMemory.Location = new System.Drawing.Point(567, 330);
+            this.btnMemory.Name = "btnMemory";
+            this.btnMemory.Size = new System.Drawing.Size(105, 33);
+            this.btnMemory.TabIndex = 2;
+            this.btnMemory.Text = "MEMORY (Off)";
+            this.btnMemory.UseVisualStyleBackColor = true;
+            this.btnMemory.Click += new System.EventHandler(this.btnMemory_Click);
+            // 
+            // btnDisk
+            // 
+            this.btnDisk.Location = new System.Drawing.Point(567, 369);
+            this.btnDisk.Name = "btnDisk";
+            this.btnDisk.Size = new System.Drawing.Size(105, 33);
+            this.btnDisk.TabIndex = 3;
+            this.btnDisk.Text = "DISK (Off)";
+            this.btnDisk.UseVisualStyleBackColor = true;
+            this.btnDisk.Click += new System.EventHandler(this.btnDisk_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 446);
+            this.Controls.Add(this.btnDisk);
+            this.Controls.Add(this.btnMemory);
+            this.Controls.Add(this.btnCPU);
+            this.Controls.Add(this.sysChart);
+            this.Name = "Main";
+            this.Text = "System Metrics";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sysChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
+        private System.Windows.Forms.Button btnCPU;
+        private System.Windows.Forms.Button btnMemory;
+        private System.Windows.Forms.Button btnDisk;
+    }
+}
+
